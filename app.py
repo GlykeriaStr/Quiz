fa4faf3b0c4a2078ac7227eeac0f3c6e892e0142
Glykeria Stravodimou <strglykeria@gmail.com>
2021-04-19 18:37:08 +0100

    Quiz is ready

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import Question from Question
+from Question import Question 
 
 questions_answers = [
-  'What is the capital of Italy \n (a) Rome \n (b) Milan \n (c) Florence',
-  'What shape is a footbal? \n (a) Square \n (b) Round \n (c) Rectangle',
-  'What colour are apples? \n (a) Yellow \n (b) Red \n (c) Black'
+  'What is the capital of Italy \n (a) Rome \n (b) Milan \n (c) Florence \n',
+  'What shape is a footbal? \n (a) Square \n (b) Round \n (c) Rectangle \n',
+  'What colour are apples? \n (a) Yellow \n (b) Red \n (c) Black \n'
 ]
 
 questions = [
@@ -12,3 +12,12 @@ questions = [
   Question(questions_answers[2], 'b')
 ]
 
+def run_app(questions):
+  score = 0
+  for question in questions:
+    answer = input(question.prompt)
+    if answer == question.answer:
+      score += 1
+  print('You scored ' + str(score) + '/' + str(len(questions)) + ' correct answers.')
+
+run_app(questions)
