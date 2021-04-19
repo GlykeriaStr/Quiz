178007422d7dc2f3a262347468c516e4e006b46a
Glykeria Stravodimou <strglykeria@gmail.com>
2021-04-19 18:23:55 +0100

    Created question objects in the app

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import Question from Question
 
 questions_answers = [
   'What is the capital of Italy \n (a) Rome \n (b) Milan \n (c) Florence',
-  'What shape is a footbal? \n (a) Round \n (b) Square \n (c) Rectangle',
-  'What colour are apples? \n (a) Red \n (b) Yellow \n (c) Black'
+  'What shape is a footbal? \n (a) Square \n (b) Round \n (c) Rectangle',
+  'What colour are apples? \n (a) Yellow \n (b) Red \n (c) Black'
 ]
+
+questions = [
+  Question(questions_answers[0], 'a'),
+  Question(questions_answers[1], 'b'),
+  Question(questions_answers[2], 'b')
+]
+
