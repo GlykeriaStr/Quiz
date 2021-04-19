a69cbfcf0e4af83ac24bb73f11e320dc80636247
Glykeria Stravodimou <strglykeria@gmail.com>
2021-04-19 18:21:40 +0100

    Created question class and some questions

---
+++ app.py
@@ -0,0 +1,7 @@
+import Question from Question
+
+questions_answers = [
+  'What is the capital of Italy \n (a) Rome \n (b) Milan \n (c) Florence',
+  'What shape is a footbal? \n (a) Round \n (b) Square \n (c) Rectangle',
+  'What colour are apples? \n (a) Red \n (b) Yellow \n (c) Black'
+]
